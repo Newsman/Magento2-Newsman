3c5fad087b41e41eb5ee161d993cced5093c26fe
lucianfcorodeanu@gmail.com <lucianfcorodeanu@gmail.com>
2021-01-25 16:57:53 +0200

    Improvements

--- app/code/Dazoot/Newsmanmarketing/view/frontend/web/js/enhanced_ecommerce.js
+++ app/code/Dazoot/Newsmanmarketing/view/frontend/web/js/enhanced_ecommerce.js
@@ -6,18 +6,9 @@ define(["jquery"], function (t) {
                 for (var n in t) {
                     c++;
                     t[n].manufacturer_name;
-                    /*ga("ec:addImpression", {
-                     id: t[n].tvc_id,
-                     name: t[n].tvc_name,
-                     category: t[n].tvc_c,
-                     price: t[n].tvc_p,
-                     list: "Category Page",
-                     position: parseInt(n) + 1
-                     }), e > 6 ? c % 6 == 0 && (e -= 6, ga("send", "event", "Enhanced-Ecommerce", "load", "product_impression_cp", {nonInteraction: 1})) : (e--, 0 == e && ga("send", "event", "Enhanced-Ecommerce", "load", "product_impression_cp", {nonInteraction: 1}))*/
-
-
+           
                     _nzm.run('ec:addImpression', {
-                        'id': t[n].tvc_id,
+                        'id': t[n].tvc_i,
                         'name': t[n].tvc_name,
                         'category': t[n].tvc_c,
                         'price': t[n].tvc_p,
@@ -33,17 +24,9 @@ define(["jquery"], function (t) {
                 jQuery(".product-item-photo, .product-item-link").on("click", function () {
                     var c = jQuery(this).siblings().find(".price-final_price").attr("data-product-id");
                     for (var e in t) {
-
-                        /*t[e].tvc_i == c && (ga("ec:addProduct", {
-                         id: t[e].tvc_id,
-                         name: t[e].tvc_name,
-                         category: t[e].tvc_c,
-                         position: parseInt(e)
-                         }), ga("ec:setAction", "click", {list: "Category Page"}), ga("send", "event", "Enhanced-Ecommerce", "click", "product_click", {nonInteraction: 1}))
-                         */
-
+     
                         _nzm.run('ec:addProduct', {
-                            'id': t[e].tvc_id,
+                            'id': t[e].tvc_i,
                             'name': t[e].tvc_name,
                             'category': t[e].tvc_c,
                             'position': parseInt(e)
@@ -69,37 +52,23 @@ define(["jquery"], function (t) {
                 wait_to_load_and_identify();
 
             }, this.tvc_pro_detail = function (t) {
-                /*ga("ec:addProduct", {
-                 id: t.tvc_id,
-                 name: t.tvc_name,
-                 category: t.tvc_c,
-                 price: t.tvc_p
-                 }), ga("ec:setAction", "detail", {list: t.tvc_list}), ga("send", "event", "Enhanced-Ecommerce", "load", "product_detail", {nonInteraction: 1})*/
-
+                
                 _nzm.run('ec:addProduct', {
-                    'id': t.tvc_id,
+                    'id': t.tvc_i,
                     'name': t.tvc_name,
                     'category': t.tvc_c,
                     price: t.tvc_p,
                     list: "Product Page",
                 });
 
-                //_nzm.run('send', 'pageview', jQuery(location).attr('pathname'), 'pageview', 'Product Page');
                 _nzm.run('ec:setAction', 'detail');
                 _nzm.run('send', 'pageview');
 
             }, this.tvc_add_to_cart = function (t) {
                 jQuery("#product-addtocart-button").on("click", function () {
-                    /* ga("ec:addProduct", {
-                     id: t.tvc_id,
-                     name: t.tvc_name,
-                     category: t.tvc_c,
-                     price: t.tvc_p,
-                     quantity: jQuery("#qty").val()
-                     }), ga("ec:setAction", "add", {list: t.tvc_list}), ga("send", "event", "Enhanced-Ecommerce", "add_to_cart", "product_add_to_cart", {nonInteraction: 1})*/
 
                     _nzm.run('ec:addProduct', {
-                        id: t.tvc_id,
+                        id: t.tvc_i,
                         name: t.tvc_name,
                         category: t.tvc_c,
                         price: t.tvc_p,
@@ -119,7 +88,7 @@ define(["jquery"], function (t) {
                     for (var n in t) {
 
                         _nzm.run('ec:addProduct', {
-                            'id': t[n].tvc_id,
+                            'id': t[n].tvc_i,
                             'quantity': e
                         });
                     }
@@ -130,22 +99,15 @@ define(["jquery"], function (t) {
                 })
             }, this.tvc_transaction_call = function (t, c) {
                 for (var e in t)_nzm.run("ec:addProduct", {
-                    id: t[e].tvc_id,
+                    id: t[e].tvc_i,
                     name: t[e].tvc_name,
                     category: t[e].tvc_c,
                     price: t[e].tvc_p,
                     quantity: t[e].tvc_Qty
-                });
-                /*ga("ec:setAction", "purchase", {
-                 id: c.tvc_id,
-                 affiliation: c.tvc_affiliate,
-                 revenue: c.tvc_revenue,
-                 tax: c.tvc_tt,
-                 shipping: c.tvc_ts
-                 }), ga("send", "event", "Enhanced-Ecommerce", "load", "order_confirmation", {nonInteraction: 1})*/
+                });               
 
                 _nzm.run('ec:setAction', 'purchase', {
-                    'id': c.tvc_id,
+                    'id': c.tvc_i,
                     'affiliation': c.tvc_affiliate,
                     'revenue': c.tvc_revenue,
                     'tax': c.tvc_tt,
@@ -156,7 +118,7 @@ define(["jquery"], function (t) {
 
                 for (var c in t) {
                     _nzm.run("ec:addProduct", {
-                        id: t[c].tvc_id,
+                        id: t[c].tvc_i,
                         name: t[c].tvc_name,
                         category: t[c].tvc_c,
                         price: t[c].tvc_p,
