1d2c3d926f05974b9907ebb0b9d9a9266201f003
lucianfcorodeanu@gmail.com <lucianfcorodeanu@gmail.com>
2022-10-19 14:14:33 +0300

    APIPrices

--- app/code/Dazoot/Newsman/Controller/Index/Magento23x.php
+++ app/code/Dazoot/Newsman/Controller/Index/Magento23x.php
@@ -263,7 +263,7 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
                         $url = $prodObjManager->getProductUrl();
                         $image_url = $imageHelper->getImageUrl($prodObjManager);        
 
-                        $priceOld = $prod->getPriceInfo()->getPrice('special_price')->getValue();
+                        $priceOld = $prod->getPriceInfo()->getPrice('regular_price')->getValue();
                         $price = $prod->getPriceInfo()->getPrice('final_price')->getValue();
 
                         if(!empty($prod->getPriceInfo()->getPrice('special_price')->getValue()))
