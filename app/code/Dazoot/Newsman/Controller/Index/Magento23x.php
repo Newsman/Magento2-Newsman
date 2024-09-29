9e968504fef53fcb8beae56fb17d23db3f71d33e
ilkadintelligence <ilkadintelligence@gmail.com>
2024-09-29 21:39:12 +0300

    allow API GET & POST

--- app/code/Dazoot/Newsman/Controller/Index/Magento23x.php
+++ app/code/Dazoot/Newsman/Controller/Index/Magento23x.php
@@ -121,7 +121,15 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
     public function NewsmanFetch($_apikey)
     {
         $apikey = (empty($_GET["apikey"])) ? "" : $_GET["apikey"];
+        if(empty($apikey))
+        {
+            $apikey = empty($_POST['nzmhash']) ? '' : $_POST['nzmhash'];
+        }
         $newsman = (empty($_GET["newsman"])) ? "" : $_GET["newsman"];
+        if(empty($newsman))
+        {
+            $newsman = empty($_POST['newsman']) ? '' : $_POST['newsman'];
+        }	    
         $start = (!empty($_GET["start"]) && $_GET["start"] >= 0) ? $_GET["start"] : 1;
         $limit = (empty($_GET["limit"])) ? 1000 : $_GET["limit"];        
         $order_id = (empty($_GET["order_id"])) ? "" : $_GET["order_id"];
@@ -498,6 +506,35 @@ class Index extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
                             $prefix = $this->getRequest()->getParam('prefix', '');
                             $expireDate = $this->getRequest()->getParam('expire_date');
                             $minAmount = (float)$this->getRequest()->getParam('min_amount', -1);
+
+			if(empty($discountType))
+			{
+			    $discountType = empty($_POST['type']) ? '' : $_POST['type'];
+			}			    
+			if(empty($value))
+			{
+			    $value = empty($_POST['value']) ? '' : $_POST['value'];
+			}			    
+			if(empty($batch_size))
+			{
+			    $batch_size = empty($_POST['batch_size']) ? '' : $_POST['batch_size'];
+			}			    
+			if(empty($prefix))
+			{
+			    $prefix = empty($_POST['prefix']) ? '' : $_POST['prefix'];
+			}			    
+			if(empty($expire_date))
+			{
+			    $expire_date = empty($_POST['expire_date']) ? '' : $_POST['expire_date'];
+			}			    
+			if(empty($min_amount))
+			{
+			    $min_amount = empty($_POST['min_amount']) ? '' : $_POST['min_amount'];
+			}			    
+			if(empty($currency))
+			{
+			    $currency = empty($_POST['currency']) ? '' : $_POST['currency'];
+			}				
 
                             if ($discountType == -1) {
                                 return $resultJson->setData(['status' => 0, 'msg' => 'Missing type param']);
