487e7d749d6640439c7d3914d6361741407f9413
Daniel Ifrim <daniel@unifiedarts.de>
2025-06-12 16:39:13 +0300

    Remove HTTP_CF_CONNECTING_IP HTTP_INCAP_CLIENT_IP
    (cherry picked from commit 586e91458bd9db2101aaf6441aab437bfd84ca68)

---
+++ src/module-dazoot-newsman/Model/System/Config/Backend/ValueMap.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Copyright © Dazoot Software S.R.L. rights reserved.
+ * See LICENSE.txt for license details.
+ *
+ * @website https://www.newsman.ro/
+ */
+namespace Dazoot\Newsman\Model\System\Config\Backend;
+
+use Dazoot\Newsman\Helper\Config\MapAbstract;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Config\Value;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
+
+/**
+ * Backend for serialized array data
+ */
+class ValueMap extends Value
+{
+    /**
+     * @var MapAbstract
+     */
+    protected $mapHelper = null;
+
+    /**
+     * @param Context $context
+     * @param Registry $registry
+     * @param ScopeConfigInterface $config
+     * @param TypeListInterface $cacheTypeList
+     * @param MapAbstract $mapHelper
+     * @param AbstractResource $resource
+     * @param AbstractDb $resourceCollection
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        ScopeConfigInterface $config,
+        TypeListInterface $cacheTypeList,
+        MapAbstract $mapHelper,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $config,
+            $cacheTypeList,
+            $resource,
+            $resourceCollection,
+            $data
+        );
+
+        $this->mapHelper = $mapHelper;
+    }
+
+    /**
+     * Process data after load
+     *
+     * @return $this
+     */
+    protected function _afterLoad()
+    {
+        $value = $this->getValue();
+        $value = $this->mapHelper->makeArrayFieldValue($value);
+        $this->setValue($value);
+
+        return $this;
+    }
+
+    /**
+     * Prepare data before save
+     *
+     * @return $this
+     */
+    public function beforeSave()
+    {
+        $value = $this->getValue();
+        $value = $this->mapHelper->makeStorableArrayFieldValue($value);
+        $this->setValue($value);
+
+        return $this;
+    }
+}
