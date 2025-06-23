83ff0edcb9275980dec8ffc2b2c2bb85cd61d717
Daniel Ifrim <daniel@unifiedarts.de>
2025-06-23 23:52:51 +0300

    Add customer attributes map
    (cherry picked from commit 01965bd8f24ca7104a7f8b2a66c4fbe1b6e5826a)

---
+++ src/module-dazoot-newsman/Model/Config/Source/Customer/Attributes.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Copyright © Dazoot Software S.R.L. rights reserved.
+ * See LICENSE.txt for license details.
+ *
+ * @website https://www.newsman.ro/
+ */
+namespace Dazoot\Newsman\Model\Config\Source\Customer;
+
+use Magento\Customer\Api\CustomerMetadataInterface;
+use Magento\Framework\Data\OptionSourceInterface;
+
+class Attributes implements OptionSourceInterface
+{
+    /**
+     * @var CustomerMetadataInterface
+     */
+    protected $customerMetadata;
+
+    /**
+     * @param CustomerMetadataInterface $customerMetadata
+     */
+    public function __construct(
+        CustomerMetadataInterface $customerMetadata
+    ) {
+        $this->customerMetadata = $customerMetadata;
+    }
+
+    /**
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * @param boolean $isMultiselect
+     * @return array
+     */
+    public function toOptionArray($isMultiselect = false)
+    {
+        if (!$this->options) {
+            $options = [];
+            $attributesMetadata = $this->customerMetadata->getAllAttributesMetadata();
+            foreach ($attributesMetadata as $attributeMetadata) {
+                $options[] = [
+                    'value' => $attributeMetadata->getAttributeCode(),
+                    'label' => $attributeMetadata->getFrontendLabel() . ' ('
+                        . $attributeMetadata->getAttributeCode() . ')'
+                ];
+            }
+            $this->options = $this->sortOptionArray($options);
+        }
+
+        if (!$isMultiselect) {
+            array_unshift($options, ['value' => '', 'label' => __('Please Select Attribute')]);
+        }
+
+        return $this->options;
+    }
+
+    /**
+     * @param array $option
+     * @return array
+     */
+    protected function sortOptionArray($option)
+    {
+        $data = [];
+        foreach ($option as $item) {
+            $data[$item['value']] = $item['label'];
+        }
+        asort($data);
+        $option = [];
+        foreach ($data as $key => $label) {
+            $option[] = ['value' => $key, 'label' => $label];
+        }
+        return $option;
+    }
+}
