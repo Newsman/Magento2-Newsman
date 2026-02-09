0de890b7dc92b3df81bf04320c37aa7338ef6521
Daniel Ifrim <daniel@unifiedarts.de>
2026-02-09 19:19:08 +0200

    Update remarketing URLs if exists in core_config_data

---
+++ src/module-dazoot-newsman-marketing/Setup/Patch/Data/UpdateConfigValues.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Copyright © Dazoot Software S.R.L. rights reserved.
+ * See LICENSE.txt for license details.
+ *
+ * @website https://www.newsman.ro/
+ */
+
+namespace Dazoot\Newsmanmarketing\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+/**
+ * Update config values in core_config_data
+ */
+class UpdateConfigValues implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $configData = [
+            'newsmanmarketing/tracking/script_url' => 'https://t.newsmanapp.com/jt/t.js',
+            'newsmanmarketing/http/resources_url' => 'https://t.newsmanapp.com/',
+            'newsmanmarketing/http/tracking_url' => 'https://rtrack.newsmanapp.com/',
+            'newsmanmarketing/http/required_file_patterns' => "jt/t.js\njt/nzm_custom_{{api_key}}.js\njt/ecommerce.js\njt/modal_{{api_key}}.js",
+        ];
+
+        $tableName = $this->moduleDataSetup->getTable('core_config_data');
+        foreach ($configData as $path => $value) {
+            $this->moduleDataSetup->getConnection()->update(
+                $tableName,
+                ['value' => $value],
+                ['path = ?' => $path]
+            );
+        }
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
