5bcc6f8e7c4b947f1ad3c425c3205abbf37555b4
Daniel Ifrim <daniel@unifiedarts.de>
2026-02-11 21:11:47 +0200

    Add wizard in admin to configure Newsman
    (cherry picked from commit faa7f0bbad333e38d74585a0a0b0b0c2083ee127)

--- src/module-dazoot-newsman/Block/Adminhtml/System/Config/Info.php
+++ src/module-dazoot-newsman/Block/Adminhtml/System/Config/Info.php
@@ -8,12 +8,30 @@
 namespace Dazoot\Newsman\Block\Adminhtml\System\Config;
 
 use Magento\Backend\Block\AbstractBlock;
+use Magento\Backend\Block\Context;
 use Magento\Framework\Data\Form\Element\AbstractElement;
 use Magento\Framework\Data\Form\Element\Renderer\RendererInterface;
 
 class Info extends AbstractBlock implements
     RendererInterface
 {
+    /**
+     * @var \Magento\Framework\App\RequestInterface
+     */
+    protected $_request;
+
+    /**
+     * @param Context $context
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->_request = $context->getRequest();
+    }
+
     /**
      * Render form element as HTML
      *
@@ -24,26 +42,47 @@ class Info extends AbstractBlock implements
     {
         $logoUrl = 'http://s1-cdn.nl.nzmt.eu/images/templates/lcor/Images/NewsmanLogo/logo_white.png';
         $contactText = __('If you need support or have any questions, please contact us at');
+        $buttonLabel = __('Configure with Newsman Login');
+
+        $params = [];
+        if ($website = (string)$this->_request->getParam('website')) {
+            $params['website'] = $website;
+        }
+        if ($store = (string)$this->_request->getParam('store')) {
+            $params['store'] = $store;
+        }
+        $loginUrl = $this->getUrl('newsman/system_config/login', $params);
 
         $html = <<<HTML
 <div style="width: 100%; padding: 15px; display: none;" id="infoPanel">
 <span style="display: inline-block; color: #49e249; padding: 5px;" id="msgType"></span>
 <button id="closeInfoPanel" style="display: inline-block; background: #a04747; color: #fff;" type="button">X</button>
 </div>
-<div style="border:1px solid #e3e3e3; min-height:100px; display;block;
-padding:15px 15px 15px 15px;">
-<p style="background: #a04747; border-radius: 10px;">
-<img src="$logoUrl" style="padding: 5px;" />
-</p>
-<p><strong>Newsman</strong> by <strong><a href="https://www.newsman.ro" target="_blank">Newsman</a></strong><br /></p>
-<p>
-<br />Like and follow us on
-<a href="http://www.facebook.com/NewsmanApp" target="_blank">Facebook</a>,
-<a href="https://www.linkedin.com/company/newsman-app" target="_blank">LinkedIn</a> and
-<a href="http://twitter.com/NewsmanApp" target="_blank">Twitter</a>.<br />
-{$contactText}
-<a href="mailto:info@newsman.ro">info@newsman.ro</a>.
-</p>
+<div style="border:1px solid #e3e3e3; min-height:100px; display: block; padding:15px; background-color: #f8f8f8; border-radius: 5px; margin-bottom: 20px;">
+    <div style="display: flex; align-items: center; margin-bottom: 15px;">
+        <div style="background: #a04747; border-radius: 5px; padding: 10px; margin-right: 15px;">
+            <img src="$logoUrl" style="display: block; height: 30px;" />
+        </div>
+        <div>
+            <h2 style="margin: 0; color: #333;">Newsman</h2>
+            <p style="margin: 0;">by <strong><a href="https://www.newsman.ro" target="_blank" style="color: #a04747; text-decoration: none;">Newsman</a></strong></p>
+        </div>
+    </div>
+    <div style="margin-bottom: 15px;">
+        <p>Like and follow us on
+            <a href="http://www.facebook.com/NewsmanApp" target="_blank" style="color: #a04747;">Facebook</a>,
+            <a href="https://www.linkedin.com/company/newsman-app" target="_blank" style="color: #a04747;">LinkedIn</a> and
+            <a href="http://twitter.com/NewsmanApp" target="_blank" style="color: #a04747;">Twitter</a>.
+        </p>
+        <p>
+            {$contactText} <a href="mailto:info@newsman.ro" style="color: #a04747; font-weight: bold;">info@newsman.ro</a>.
+        </p>
+    </div>
+    <div style="margin-top: 20px;">
+        <a href="{$loginUrl}" class="action-default scalable primary">
+            <span>{$buttonLabel}</span>
+        </a>
+    </div>
 </div>
 HTML;
         return $html;
---
+++ src/module-dazoot-newsman/Controller/Adminhtml/System/Config/TestCredentials.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Copyright © Dazoot Software S.R.L. rights reserved.
+ * See LICENSE.txt for license details.
+ *
+ * @website https://www.newsman.ro/
+ */
+namespace Dazoot\Newsman\Controller\Adminhtml\System\Config;
+
+use Dazoot\Newsman\Model\Config as NewsmanConfig;
+use Dazoot\Newsman\Model\Service\Configuration\GetListAll;
+use Dazoot\Newsman\Model\Service\Context\Configuration\UserContextFactory;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Controller to test Newsman credentials by attempting to fetch lists
+ */
+class TestCredentials extends Action
+{
+    /**
+     * Authorization level of a basic admin session.
+     */
+    public const ADMIN_RESOURCE = 'Dazoot_Newsman::config_newsman';
+
+    /**
+     * @var GetListAll
+     */
+    protected $getListAll;
+
+    /**
+     * @var UserContextFactory
+     */
+    protected $userContextFactory;
+
+    /**
+     * @var NewsmanConfig
+     */
+    protected $config;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @param Context $context
+     * @param GetListAll $getListAll
+     * @param UserContextFactory $userContextFactory
+     * @param NewsmanConfig $config
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        Context $context,
+        GetListAll $getListAll,
+        UserContextFactory $userContextFactory,
+        NewsmanConfig $config,
+        StoreManagerInterface $storeManager
+    ) {
+        parent::__construct($context);
+        $this->getListAll = $getListAll;
+        $this->userContextFactory = $userContextFactory;
+        $this->config = $config;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * Execute: try to fetch lists using configured credentials and show a notice
+     *
+     * @return Redirect
+     */
+    public function execute()
+    {
+        $params = [];
+        $storeParam = (string)$this->getRequest()->getParam('store');
+        $websiteParam = (string)$this->getRequest()->getParam('website');
+        if (!empty($websiteParam)) {
+            $params['website'] = $websiteParam;
+        }
+        if (!empty($storeParam)) {
+            $params['store'] = $storeParam;
+        }
+
+        // Resolve an effective store scope for reading values
+        $effectiveStore = null;
+        try {
+            if (!empty($storeParam)) {
+                $effectiveStore = $this->storeManager->getStore($storeParam)->getId();
+            } elseif (!empty($websiteParam)) {
+                $effectiveStore = $this->storeManager->getWebsite($websiteParam)->getDefaultStore()->getId();
+            }
+        } catch (\Exception $e) {
+            // leave $effectiveStore as null (default scope)
+        }
+
+        $userId = (int)$this->config->getUserId($effectiveStore);
+        $apiKey = (string)$this->config->getApiKey($effectiveStore);
+
+        $success = false;
+        if ($userId && $apiKey) {
+            try {
+                $this->getListAll->execute(
+                    $this->userContextFactory->create()
+                        ->setUserId($userId)
+                        ->setApiKey($apiKey)
+                );
+                $success = true;
+            } catch (LocalizedException $e) {
+                $success = false;
+            } catch (\Throwable $t) {
+                $success = false;
+            }
+        }
+
+        if ($success) {
+            $this->messageManager->addSuccessMessage(__('Credentials are valid'));
+        } else {
+            $this->messageManager->addErrorMessage(__('Credentials are invalid or there is temporarily API error!'));
+        }
+
+        /** @var Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setPath('adminhtml/system_config/edit', ['section' => 'newsman'] + $params);
+        return $resultRedirect;
+    }
+}
